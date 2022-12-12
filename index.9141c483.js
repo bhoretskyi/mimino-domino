67db3584efd82329468017fa28c1651964a2a6b7
bhoretskyi <111376936+bhoretskyi@users.noreply.github.com>
2022-12-12 11:11:07 +0000

    Deploying to gh-pages from @ bhoretskyi/mimino-domino@740529223fb09506f3f4a53a965116eb772b9174 🚀

---
+++ index.9141c483.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("visually-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.9141c483.js.map
