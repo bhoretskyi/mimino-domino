1a6f172618f47b688d676b7449f560ca0da66aea
bhoretskyi <111376936+bhoretskyi@users.noreply.github.com>
2022-12-12 08:17:19 +0000

    Deploying to gh-pages from @ bhoretskyi/mimino-domino@0fffd34b033754fe1164e0fe146c8c16cb6f5c44 🚀

---
+++ hotel.022b7052.js
@@ -0,0 +1,2 @@
+$(".one-time").slick({dots:!1,infinite:!0,speed:300,slidesToShow:2,adaptiveHeight:!0});
+//# sourceMappingURL=hotel.022b7052.js.map
---
+++ hotel.9d7222b9.js
@@ -0,0 +1,2 @@
+$(".one-time").slick({dots:!1,infinite:!0,speed:300,slidesToShow:2,adaptiveHeight:!0});
+//# sourceMappingURL=hotel.9d7222b9.js.map
