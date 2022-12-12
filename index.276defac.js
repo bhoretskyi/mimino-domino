8722503d732d92a65ed305fc3f69881f8fb1e948
bhoretskyi <111376936+bhoretskyi@users.noreply.github.com>
2022-12-12 21:00:37 +0000

    Deploying to gh-pages from @ bhoretskyi/mimino-domino@36b17035aba180ab9a221eb4111a7194c18a5f70 🚀

--- index.276defac.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={openModalBtn:document.querySelector("[data-modal-room-open]"),closeModalBtn:document.querySelector("[data-modal-room-close]"),modal:document.querySelector("[data-modal-room]")};function e(){o.modal.classList.toggle("visually-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
-//# sourceMappingURL=index.276defac.js.map
---
+++ index.864a2cfc.js
@@ -0,0 +1,2 @@
+$(".about-restaurant_slider").slick({dots:!1,infinite:!0,speed:300,slidesToShow:2,adaptiveHeight:!0,variableWidth:!0,arrows:!0}),$(".one-time").slick({dots:!1,infinite:!0,speed:300,slidesToShow:2,adaptiveHeight:!0}),$(".about-hotel-slick").slick({dots:!1,infinite:!0,speed:300,slidesToShow:2,adaptiveHeight:!0});
+//# sourceMappingURL=index.864a2cfc.js.map
