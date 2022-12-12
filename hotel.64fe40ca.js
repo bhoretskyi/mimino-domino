a61c9f78c4dda16092721ccbaae8bccf9946e329
bhoretskyi <111376936+bhoretskyi@users.noreply.github.com>
2022-12-12 11:33:57 +0000

    Deploying to gh-pages from @ bhoretskyi/mimino-domino@0d6b15edf388dbf1a57b0130ba71f00142f5ad1f 🚀

---
+++ hotel.64fe40ca.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-price-open]"),closeModalBtn:document.querySelector("[data-modal-price-close]"),modal:document.querySelector("[data-price-room]")};function o(){e.modal.classList.toggle("visually-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=hotel.64fe40ca.js.map
---
+++ index.276defac.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-modal-room-open]"),closeModalBtn:document.querySelector("[data-modal-room-close]"),modal:document.querySelector("[data-modal-room]")};function e(){o.modal.classList.toggle("visually-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.276defac.js.map
