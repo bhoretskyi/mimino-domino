e521e5e1561016a6fc15e40523541150b1fc16a1
bhoretskyi <111376936+bhoretskyi@users.noreply.github.com>
2022-12-13 20:13:03 +0000

    Deploying to gh-pages from @ bhoretskyi/mimino-domino@8644529ab86fdf1cba537223f756af96b3d24886 🚀

--- index.a1e75127.js
+++
@@ -1,2 +0,0 @@
-$(".about-restaurant_slider").slick({dots:!1,infinite:!0,speed:300,slidesToShow:2,adaptiveHeight:!0,variableWidth:!0,arrows:!0}),$(".lux-time").slick({dots:!1,arrows:!1,infinite:!0,speed:300,slidesToShow:2,slidesToScroll:2,adaptiveHeight:!0,responsive:[{breakpoint:1024,settings:{slidesToShow:1.5,slidesToScroll:1}}]}),$(".left").on("click",(function(){$(".lux-time").slick("slickPrev")})),$(".right").on("click",(function(){$(".lux-time").slick("slickNext")}));
-//# sourceMappingURL=index.a1e75127.js.map
