66817d73267aa19efab88da3c8be38248edb1ec3
bhoretskyi <111376936+bhoretskyi@users.noreply.github.com>
2022-12-13 07:32:56 +0000

    Deploying to gh-pages from @ bhoretskyi/mimino-domino@633312384cab3c27f4d3e3bf1cde914d70786f04 🚀

--- index.8e71a22d.js
+++
@@ -1,2 +0,0 @@
-$(".about-restaurant_slider").slick({dots:!1,infinite:!0,speed:300,slidesToShow:2,adaptiveHeight:!0,variableWidth:!0,arrows:!0}),$(".one-time").slick({dots:!1,infinite:!0,speed:300,slidesToShow:2,adaptiveHeight:!0}),$(".about-hotel-slick").slick({dots:!1,infinite:!0,speed:300,slidesToShow:2,adaptiveHeight:!0});
-//# sourceMappingURL=index.8e71a22d.js.map
