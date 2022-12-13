4d6821d9332802a6c6a915f139ff98ca530b039a
Sazhin-Kostya <sazhinkostya@gmail.com>
2022-12-13 14:42:09 +0200

    Закінчив деск-топ

---
+++ src/js/about-hotel.js
@@ -0,0 +1,7 @@
+$('.about-hotel-slick').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 2,
+  adaptiveHeight: true,
+});
--- src/js/slick.js
+++ src/js/slick.js
@@ -31,10 +31,3 @@ $('.right').on("click", function () {
   $('.lux-time').slick('slickNext');
 });
 
-$('.about-hotel-slick').slick({
-  dots: false,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 2,
-  adaptiveHeight: true,
-});
