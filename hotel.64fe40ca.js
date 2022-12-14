71a79278484aa593eabb8f9d560f8c7544415ea5
bhoretskyi <111376936+bhoretskyi@users.noreply.github.com>
2022-12-14 00:32:04 +0000

    Deploying to gh-pages from @ bhoretskyi/mimino-domino@c9ad592a2249e7c5bad6c65caedf48c6d2901eb3 🚀

--- hotel.64fe40ca.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openModalBtn:document.querySelector("[data-modal-price-open]"),closeModalBtn:document.querySelector("[data-modal-price-close]"),modal:document.querySelector("[data-price-room]")};function o(){e.modal.classList.toggle("visually-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
-//# sourceMappingURL=hotel.64fe40ca.js.map
