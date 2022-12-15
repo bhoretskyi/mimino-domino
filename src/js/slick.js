42bda916a40f8274a0746a46dbe0f4d72b8bf1b3
Sazhin-Kostya <sazhinkostya@gmail.com>
2022-12-15 15:22:59 +0200

    новый код

--- src/js/slick.js
+++ src/js/slick.js
@@ -106,3 +106,11 @@ $(document).ready(function () {
   });
 });
 
+$('.about-hotel_slider').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 2,
+  adaptiveHeight: true,
+});
+
